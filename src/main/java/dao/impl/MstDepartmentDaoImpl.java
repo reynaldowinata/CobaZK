c4bb0069584b44873427b2db00a7b71030552bb8
kaizan1412 <urameshi.yusuke75@gmail.com>
2019-05-27 15:27:52 +0700

    custom dao moved

--- src/main/java/dao/impl/MstDepartmentDaoImpl.java
+++ src/main/java/dao/impl/MstDepartmentDaoImpl.java
@@ -7,7 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
-import dao.MstDepartmentCustomDao;
+import dao.custom.MstDepartmentCustomDao;
 import entity.MstDepartment;
 
 public class MstDepartmentDaoImpl implements MstDepartmentCustomDao{
